f27850a6b82ebe4d263a5d761cecc7d4fce65929
Schmidt László <schmidtlacus@gmail.com>
2019-12-14 13:57:27 +0100

    proglecke

---
+++ C#/prog_lecke/Functions/Functions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions
+{
+    class Functions
+    {
+        public int buzibb(int r)
+        {
+            return 0;
+        }
+        public double körker(double r)
+        {
+            return 2 * r * pi();
+        }
+        double pi()
+        {
+            return 3.1415926535;
+        }
+        public double negyker(int a, int b)
+        {
+            return (a + b) * 2;
+        }
+
+        public double lear(int ar ,int szazalek ,Action F)
+        {
+            F();
+
+            if (szazalek > 100)
+                return 0;
+            if (szazalek < 0)
+                return ar;
+            double ujar;
+            ujar = ar - (ar * (double) szazalek / 100.0);
+
+            return ujar;
+        }
+
+
+    }
+
+}
---
+++ C#/prog_lecke/Functions/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {/*
+                Console.WriteLine("Functions...");
+
+                Say();
+
+                Action F = () => { Console.WriteLine("asd"); };
+
+                List<elem> lista = new List<elem>();
+
+            lista.Sort(delegate (elem elem1, elem elem2) { return elem1.a.CompareTo(elem2.a); });
+
+                F();*/
+            }
+
+
+            Console.WriteLine("helo");
+            Functions funcszionsz = new Functions();
+            Random rng = new Random();
+            int a = rng.Next(10);
+            int b = rng.Next(10);
+            double r = 5;
+            Console.WriteLine( funcszionsz.körker(r) );
+
+
+            Console.WriteLine(funcszionsz.negyker(a, b));
+            int ar = 1000;
+
+            Action Say = () => { Console.WriteLine("saasdasda sd asd awfjnjjkhakhjn lkmklagh "); };
+            
+            Console.WriteLine(funcszionsz.lear(ar,30,Say));
+            /*Console.WriteLine(funcszionsz.lear(ar, 50));
+            Console.WriteLine(funcszionsz.lear(ar, 0));
+            Console.WriteLine(funcszionsz.lear(ar, -10));
+            Console.WriteLine(funcszionsz.lear(ar, 100));
+            Console.WriteLine(funcszionsz.lear(ar, 1200));
+            */
+            
+
+            
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+
+    }
+    
+
+}
